bbd95b1fae81907e2dde706d29230df21631320f
Adrian Legion <nlawliet6@gmail.com>
2024-02-24 21:37:41 -0500

    refactor: Update state initialization to use a Map in ListProduct component and adjust mapping over allProducts to accommodate changes

--- src/Components/ListProduct/ListProduct.jsx
+++ src/Components/ListProduct/ListProduct.jsx
@@ -4,27 +4,27 @@ import cross_icon from '../../assets/cross_icon.png'
 
 const ListProduct = () => {
 
-	const [allProducts, setAllProducts] = useState([]);
+	const [allProducts, setAllProducts] = useState(new Map());
 
 	const fetchInfo = async () => {
 		await fetch('http://localhost:4000/all_product')
-		.then( (res) => res.json() )
-		.then( (data) => {
-			setAllProducts(data.all_product)
-		} );
+			.then((res) => res.json())
+			.then((data) => {
+				setAllProducts(new Map(data.all_product))
+			});
 	}
 
-	useEffect( () => {
+	useEffect(() => {
 		fetchInfo();
 	}, [])
 
-	useEffect( () => {
+	useEffect(() => {
 		console.log(allProducts);
 	}, [allProducts])
 
 	const removeProduct = async (id) => {
 
-		if( !window.confirm('Are you sure?') ) return;
+		if (!window.confirm('Are you sure?')) return;
 
 		await fetch('http://localhost:4000/remove_product', {
 			method: 'DELETE',
@@ -32,9 +32,9 @@ const ListProduct = () => {
 				Accept: 'application/json',
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({id: id})
-		}).then( (res) => res.json() ).then( (data) => {
-			if(data.success){
+			body: JSON.stringify({ id: id })
+		}).then((res) => res.json()).then((data) => {
+			if (data.success) {
 				alert('Product removed successfully');
 				fetchInfo();
 			} else {
@@ -57,22 +57,44 @@ const ListProduct = () => {
 			</div>
 			<div className="listproduct-allproducts">
 				<hr />
-			{/* // Produt data from API */}
-			{ allProducts.map((product, index) => {
+				{/* // Produt data from API */}
+				{/* { allProducts.map((product, index) => {
 				return (
 					<React.Fragment key={index}>
 						<div className="listproduct-format-main listproduct-format">
 							<img className='listproduct-product-image' src={product.image} />
 							<p>{product.name}</p>
 							<p>{`${product.retail_price ? `\$ ${product.retail_price}` : `-`}`}</p>
-							<p>{`${product.offer_price ? `\$ ${product.offer_price}` : `-`}`}</p>
+							<p>{`${product.sale_price ? `\$ ${product.sale_price}` : `-`}`}</p>
 							<p>{product.category}</p>
 							<img onClick={()=>{removeProduct(product.id)}} className="listproduct-remove-icon" src={cross_icon} />
 						</div>
 						<hr />
 					</React.Fragment>
 				)
-			})}
+			})} */}
+				{
+					/*
+						allProduct is a MAP. They key is the id of the object, and the value is the object itself.
+						We can use the Object.entries() method to get an array of the key-value pairs of the map, and then use the map() method to map over the array.
+					*/
+					Array.from(allProducts.entries()).map(([key, product]) => {
+						console.log( product);
+						return (
+							<React.Fragment key={key}>
+								<div className="listproduct-format-main listproduct-format">
+									<img className='listproduct-product-image' src={product.image} />
+									<p>{product.name}</p>
+									<p>{`${product.retail_price ? `\$ ${product.retail_price}` : `-`}`}</p>
+									<p>{`${product.sale_price ? `\$ ${product.sale_price}` : `-`}`}</p>
+									<p>{product.category}</p>
+									<img onClick={() => { removeProduct(product.id) }} className="listproduct-remove-icon" src={cross_icon} />
+								</div>
+								<hr />
+							</React.Fragment>
+						)
+					})
+				}
 
 			</div>
 		</div>
